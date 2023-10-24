392f472aebed105ba17b18ad59891a166e033913
astrange90 <astrange90@hotmail.com>
2023-10-23 23:43:03 -0700

    Added README issues

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Codename-Spy-Kids
+Could not complete assignment because of javascript code confusion. I was lost and needed guidance on using functions correctly and making the generate button work. I will need tutoring and more practice in this area.
+![image](https://github.com/astrange90/Codename-Spy-Kids/assets/145310116/7a8796fd-0b88-476a-8bb3-3e3ca924e39e)
+
+![image](https://github.com/astrange90/Codename-Spy-Kids/assets/145310116/e3f1ea9c-19f5-4c64-894a-b7f26a6bc763)
--- script.js
+++ script.js
@@ -10,17 +10,17 @@ var passwordText = document.querySelector("#password");
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var number = "0123456789";
-  var special characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var Symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-  var password = "writePassword" = upperCase + lowerCase + number + Symbol;
+  var writePassword = upperCase + lowerCase + number + Symbol;
 
   function writePassword() {
-    if password = "" {
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
-    password += Symbol[Math.floor(Math.random() * Symbol.length)];
-    } else if (length) > password.length) {
+    if (Password) {
+    Password == upperCase[Math.floor(Math.random() * upperCase.length)];
+    Password == lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    Password == number[Math.floor(Math.random() * number.length)];
+    password == Symbol[Math.floor(Math.random() * Symbol.length)];
+    } else if (length) >= password.length) {
       password += writePassword[Math.floor(Math.random() * writePassword.length)];
     } else {
       console.log("Password did not meet criteria");
