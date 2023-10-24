a715282ad7e54e39ad922260af925d1d44a8fa03
astrange90 <astrange90@hotmail.com>
2023-10-23 22:40:41 -0700

    Added some variables to JS

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  console.log("The length of password should be at least 8 characters and no more than 128 characters")
+  
+  var password = generatePassword() {
+  
+  
+  var userChoice = window.prompt()("The length of password should be at least 8 characters and no more than 128 characters")
+  }
+
   var passwordText = document.querySelector("#password");
+  var length = 128;
+
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var special characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 
   passwordText.value = password;
 
 }
+
+var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
