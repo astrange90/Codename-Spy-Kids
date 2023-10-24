ae4d740afaa6442649c64617a90ec74cbc5708fd
astrange90 <astrange90@hotmail.com>
2023-10-23 23:10:01 -0700

    Added function for password criteria

--- script.js
+++ script.js
@@ -4,16 +4,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  console.log("The length of password should be at least 8 characters and no more than 128 characters")
-  
-  var password = generatePassword() {
-  
-  
-  var userChoice = window.prompt()("The length of password should be at least 8 characters and no more than 128 characters")
-  }
-
-  var passwordText = document.querySelector("#password");
+var passwordText = document.querySelector("#password");
   var length = 128;
 
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -21,13 +12,24 @@ function writePassword() {
   var number = "0123456789";
   var special characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+  var password = "writePassword" = upperCase + lowerCase + number + Symbol;
+
+  function writePassword() {
+    if password = "" {
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += Symbol[Math.floor(Math.random() * Symbol.length)];
+    } else if (length) > password.length) {
+      password += writePassword[Math.floor(Math.random() * writePassword.length)];
+    } else {
+      console.log("Password did not meet criteria");
+    }
+    return;
 
   passwordText.value = password;
 
-}
-
-var index = Math.floor(Math.random() * options.length);
-  var computerChoice = options[index];
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
