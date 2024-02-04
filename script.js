dcad4cf136e248805c78b6d6233e53b8d23b1f47
astrange90 <astrange90@hotmail.com>
2024-02-03 18:01:27 -0800

    added if statements for userChoices

--- script.js
+++ script.js
@@ -111,8 +111,27 @@ var upperCase = [
   }
 
   function generatePassword() {
-    var passwordOptions = getPasswordChoices();
-    
+    var passwordLength = prompt('8-128 characters')
+    var options = getPasswordChoices();
+    var userChoices = [];
+    if(options.hasSpecialChars){
+      userChoices = userChoices.concat(symbol)
+    }
+    if(options.hasLowerChars){
+      userChoices = userChoices.concat(lowerCase)
+    }
+    if(options.hasNumericChars){
+      userChoices = userChoices.concat(number)
+    }
+    if(options.hasUpperChars){
+      userChoices = userChoices.concat(upperCase)
+    }
+    var result = ''
+    for(var i = 0; i < passwordLength; i++){
+
+      result += userChoices[Math.floor(Math.random()*userChoices.length)];
+    }
+    return result;
   }
   
   // Get references to the #generate element
@@ -128,22 +147,3 @@ var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
